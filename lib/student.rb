fdd126ba2ba67f7e91a6d115a67ffe87fbf5fdae
boruch d <dubywider@gmail.com>
2019-11-26 21:19:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,30 @@ attr_accessor :name, :grade, :id
     student.save
     student
   end
-    
-    
-end
+def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    student = self.new(name, grade, id)
+    student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+     DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ?
+          WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+end	end
